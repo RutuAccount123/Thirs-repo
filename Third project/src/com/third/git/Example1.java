35a80df09536c9d0dda86103015cf7471001d25c
RutuAccount123 <shubhangikorke0452@gmail.com>
2022-11-15 17:09:51 -0800

    my third commit

---
+++ Third project/src/com/third/git/Example1.java
@@ -0,0 +1,14 @@
+package com.third.git;
+
+import java.util.ArrayList;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		ArrayList al =new ArrayList();
+		al.add(2);
+		al.add(5);
+System.out.println(al);
+	}
+
+}
